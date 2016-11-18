ec94a896140a3cffdd56e4387a9982526606edc0
tapang93 <Axeldama@Alejandros-MacBook-Pro.local>
2016-11-18 15:03:36 -0600

    implementing google sign on

--- app/src/main/java/me/jeremy/fithub/Login.java
+++ app/src/main/java/me/jeremy/fithub/Login.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class Login extends AppCompatActivity{
 
     public Button register,login;
@@ -18,15 +21,17 @@ public class Login extends AppCompatActivity{
     void init(){
         register = (Button) findViewById(R.id.login_signup);
         register.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent toRegister = new Intent(Login.this,Register.class);
-                    startActivity(toRegister);
-                }
+            @Override
+            public void onClick(View v) {
+                Intent toRegister = new Intent(Login.this,Register.class);
+                startActivity(toRegister);
             }
+        }
 
-        );
+                                    );
     }
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,19 @@ public class Login extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
 
